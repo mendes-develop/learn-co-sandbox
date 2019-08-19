365f2e812f89a3f0ec9536d97ffcecce90976333
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 22:13:05 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+# puts "Joy to the world"
+# puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+# puts "Joy to you and me"
+
+while true do
+  puts "Infinity loop"
+end
+
+puts "falsey statement"
